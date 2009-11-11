af2d278fa991d08a91233acda4e0c026778ce955
dolzenko <dolzenko@gmail.com>
2009-11-11 13:56:59 +0300

    Make FileStore#exist? behave consistently with FileStore#read

--- lib/active_support/cache/file_store_extras.rb
+++ lib/active_support/cache/file_store_extras.rb
@@ -3,13 +3,23 @@ module ActiveSupport
   module Cache
     class FileStore < Store
       alias :original_read :read
-      
+      alias :original_exist? :exist?
+
       def read(name, options = {})
         lastmod = File.mtime(real_file_path(name)) rescue nil
         if lastmod && options.is_a?(Hash) && options[:expires_in] && ((lastmod + options[:expires_in]) <= Time.now)
           nil
         else  
           original_read(name, options)
+        end
+      end
+
+      def exist?(name, options = nil)
+        lastmod = File.mtime(real_file_path(name)) rescue nil
+        if lastmod && options.is_a?(Hash) && options[:expires_in] && ((lastmod + options[:expires_in]) <= Time.now)
+          false
+        else
+          original_exist?(name, options)
         end
       end
     end
